38dc39647759e8bb54be7fc381f4136da9745021
pranavatpeaks <118458502+pranavatpeaks@users.noreply.github.com>
2025-05-28 14:48:29 +0530

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -45,11 +45,9 @@ export default function Home() {
               subscribe to <span className={styles.brand}>my instruments</span>
          </h1>
             <p className={styles.description}>
-              This is Pranav and these are <span className={styles.highlight}>my instruments</span>. 
+              This is Pranav and these are <span className={styles.highlight}>my instruments</span>. </p>
               <p> Subscribe to let <span className={styles.highlight}>my sound flow</span> through your mailboxes.</p>
             </p>
-         
-          <p></p>
           <form onSubmit={handleSubmit} className={styles.form}>
             <input
               type="text"
